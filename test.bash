de5fba94a539b4c0dc932e79592ea60ed35194c9
souki kajikawa <s24c1038st@s.chibakoudai.jp>
2025-11-06 11:46:23 +0900

    Fixed a command

--- test.bash
+++ test.bash
@@ -10,16 +10,16 @@ ng () {
 res=0
 ### NORMAL ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng "$LINENO"
+[ "${out}" = 15 ] || ng "$LINENO"
 
 ### STRANGE ###
 out=$(echo あ | ./plus)
-[ "$?" = 1.0 ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 ### STRANGE ###
 out=$(echo | ./plus)
-[ "$?" = 1.0 ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
